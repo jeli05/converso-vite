267e39e5a1f7ed4cf13cae63095e47e577d42a08
Jeff Li <jefflicj@gmail.com>
2024-01-03 14:30:45 -0800

    Convert all previous files to Typescript and temporarily remove client/server distinction

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import React, { useState } from 'react';
+import TextBox from './TextBox';
+import { fr_prompts, es_prompts, it_prompts, pt_prompts, de_prompts } from './Prompts';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Languages = {
+  [key: string]: string;
+};
+
+type Prompts = {
+  [key: string]: string;
+};
+
+function getDate(): string {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${month}/${date}/${year}`;
+}
+
+const currLang: string = 'en'; // use default of English
+const languages: Languages = {
+  'en': 'English', 
+  'fr': 'French', 
+  'es': 'Spanish', 
+  'it': 'Italian', 
+  'de': 'German', 
+  'pt': 'Portuguese'
+};
+
+function setPrompts(): void {
+  localStorage.setItem('fr_prompts', JSON.stringify(fr_prompts));
+  localStorage.setItem('es_prompts', JSON.stringify(es_prompts));
+  localStorage.setItem('it_prompts', JSON.stringify(it_prompts));
+  localStorage.setItem('pt_prompts', JSON.stringify(pt_prompts));
+  localStorage.setItem('de_prompts', JSON.stringify(de_prompts));
+}
+
+function displayPrompt(lang: string): string {
+  console.log("display lang: ", lang);
+
+  var now: Date = new Date();
+  var start: Date = new Date(now.getFullYear(), 0, 0);
+  console.log(start);
+  var diff = (now.getTime() - start.getTime()) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+  var oneDay = 1000 * 60 * 60 * 24;
+  var day = Math.floor(diff / oneDay);
+  console.log('Day of year: ' + day);
+
+  if (lang === 'fr') {
+    return fr_prompts[day];
+  } else if (lang === 'es') {
+    return es_prompts[day];
+  } else if (lang === 'it') {
+    return it_prompts[day];
+  } else if (lang === 'pt') {
+    return pt_prompts[day];
+  } else if (lang === 'de') {
+    return de_prompts[day];
+  } else {
+    return "No prompt available";
+  }
+}
+
+if (localStorage.getItem('currLang') === null) {
+  localStorage.setItem('currLang', JSON.stringify('en'));
+}
+
+console.log("Initial currLang: ", localStorage.getItem('currLang'));
+let today_prompt = displayPrompt(JSON.parse(localStorage.getItem('currLang') || 'en'));
+
+function App(): JSX.Element {
+  const [currentDate] = useState(getDate());
+  const [currentLang, setLang] = useState<string>(JSON.parse(localStorage.getItem('currLang') || 'en'));
+
+  let currLang = currentLang;
+  console.log("Refreshed currLang: ", currLang);
+
+  function changeLanguage(event: React.ChangeEvent<HTMLSelectElement>): void {
+    setLang(event.target.value);
+    currLang = event.target.value;
+    localStorage.setItem('currLang', JSON.stringify(event.target.value));
+    console.log("Switched to " + JSON.stringify(event.target.value) + ": " + languages[currLang]);
+    today_prompt = displayPrompt(event.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='prompt'>
+      <h1>Converso: Daily Language Prompts</h1>
+      <h2>{currentDate}</h2>
+      <h3>Language: {languages[currentLang]}</h3>
+      <select value={currLang} onChange={changeLanguage}>
+        {Object.keys(languages).map(lang => (
+          <option key={lang} value={lang}>{languages[lang]}</option>
+        ))}
+      </select>
+      <p>{today_prompt}</p>
+      <TextBox />
+    </div>
+  );
+
+  // const [count, setCount] = useState(0)
+
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vitejs.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
 }
 
 export default App
---
+++ src/TextBox.tsx
@@ -0,0 +1,133 @@
+// TextBox.js
+
+import React, { useState, useEffect } from 'react';
+import './TextBox.css'; // Import the CSS file for styling
+import { Sapling } from "@saplingai/sapling-js/observer";
+
+function getDate() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${month}/${date}/${year}`;
+}
+
+// initialize responses item in local storage
+// responses is array of dict
+// let responses: { date: string { lang: string : { response: string } } }[] = [];
+// [ "01-01-2024": { "de", { "postContent":"Hallo" } }, "01-02-2024": { "it", { "postContent":"Ciao" } } ]
+// let responses: { [key: string]: { language: string; postContent: string } }[] = [];
+// if (localStorage.getItem('my_responses') === null) {
+//     localStorage.setItem('my_responses', "");
+// } else {
+//     if (localStorage.getItem('my_responses')) {
+//         responses = JSON.parse(localStorage.getItem('my_responses') || "");
+//     }
+// }
+
+const TextBox: React.FC = () => {
+    // state to make text box uneditable after submit
+  const [isEditable, setIsEditable] = useState(true);
+  const [userInput, setUserInput] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUserInput(event.target.value);
+  };  
+  
+  useEffect(() => {
+    Sapling.init({
+        key: 'AR46FO1Z1P4KMK17RZ6XVO6ZAUFPW9SG',
+        endpointHostname: 'https://api.sapling.ai',
+        editPathname: '/api/v1/edits',
+        statusBadge: true,
+        mode: 'dev',
+    });
+    // Sapling.init({
+    //     // endpointHostname: 'http://127.0.0.1:5000',
+    //     // saplingPathPrefix: '/sapling',
+    //     lang: JSON.parse(localStorage.getItem("currLang") || ""), // change to selected language
+    // });
+  });
+
+  const isInputValid = userInput.length >= 70 && userInput.length <= 130; // set length constraints
+  const disableSubmit = (!isInputValid || !isEditable) ? true : false; // disable submit if not meeting length constraints or response submitted
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.currentTarget //.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    // fetch('/some-api', { method: form.method, body: formData });
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+    
+    // make text box uneditable
+    setIsEditable(false);
+    
+    const todayDate: string = getDate();
+    const currentLang: string | null = localStorage.getItem('currLang');
+    const entry: { [key: string]: { [key: string]: any } } = {};
+    const subentry: { [key: string]: any } = {};
+    subentry[currentLang as string] = formJson;
+    entry[todayDate] = subentry;
+    // let todayDate = getDate();
+    // let currentLang = localStorage.getItem('currLang');
+    // let entry = {};
+    // let subentry = {};
+    // subentry[currentLang] = formJson;
+    // entry[todayDate] = subentry;
+    console.log(entry);
+
+    const storedResponses = localStorage.getItem('my_responses');
+    let responses: Array<{ [key: string]: { [key: string]: any } }> = [];
+    if (storedResponses) {
+      responses = JSON.parse(storedResponses);
+    }
+
+    responses.push(entry);
+    localStorage.setItem('my_responses', JSON.stringify(responses));
+    console.log("responses: ", responses);
+
+    // use Sapling
+    const editor = document.getElementById('editor');
+    if (editor) {
+        Sapling.observe(editor);
+      }
+}
+
+  return (
+    <div className="textbox-container">
+        <form method="post" onSubmit={handleSubmit}>
+        <div style={{ position: 'relative' }}>
+        <label>
+            <textarea
+            name="postContent"
+            value={userInput}
+            onChange={handleInputChange}
+            rows={4}
+            cols={60}
+            style={{resize: "none", borderRadius: "8px", marginBottom: '20px'}}
+            placeholder='Type your response'
+            disabled={!isEditable}
+            id="editor"
+            />
+            <div className="char-display" style={{ color: isInputValid ? 'black' : 'red'}}>
+                {userInput.length}
+            </div>
+            <p>70-130 characters</p>
+        </label>
+        </div>
+        <hr />
+        <button type="submit" disabled={disableSubmit}>Check grammar</button>
+        </form>
+    </div>
+  );
+};
+
+export default TextBox;
